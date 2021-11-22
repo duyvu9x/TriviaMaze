b0da084f80d383c38f02ffe1dcbf2560c33e02c0
MawnsterMawth <ashzheng2000@gmail.com>
2021-11-21 17:38:57 -0800

    conflicts

--- TriviaMaze/src/Application/QuestionAnswer.java
+++ TriviaMaze/src/Application/QuestionAnswer.java
@@ -3,4 +3,7 @@ package Application;
 public class QuestionAnswer {
 	// to run the question .( i think retun true all for to 
 	// close the door)
+	private static String myQuestion;
+	
+	private static String myAnswer;
 }
