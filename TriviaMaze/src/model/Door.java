3628899444f2b0c10099358ece884c1a98b7298a
MawnsterMawth <ashzheng2000@gmail.com>
2021-11-21 22:37:19 -0800

    add door functions

--- TriviaMaze/src/model/Door.java
+++ TriviaMaze/src/model/Door.java
@@ -1,9 +1,36 @@
 package model;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Door {
 	/**
 	 * return true if it open;
 	 */
 	private boolean isOpen;
+	private String myQuestion;
+	private String myAnswers;
+	private String correctAnswer;
+
+	public String getQuestion() {
+		return myQuestion;
+	}
+
+	public void setQuestionAndAnswers() {
+		Random rand = new Random();
+		int intRandom = rand.nextInt(15) + 1;
+	}
+
+	public String getAnswer() {
+		return myAnswers;
+	}
+	
+	public void displayQuestion() {
+		System.out.println(myQuestion);
+	}
+	
+	public boolean lockDoor(String userAnswer) {
+		if (!correctAnswer.equals(userAnswer))
+			isOpen = false;
+	}
 
 }
